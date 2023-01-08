9a838df6604f812ef4b3478dba8d0fafb28476aa
Cake <cakemancerr@gmail.com>
2023-01-08 22:55:32 +0100

    refactor: extract add function to store

--- src/components/HabitCard.tsx
+++ src/components/HabitCard.tsx
@@ -1,27 +1,24 @@
 import { useStore } from "@nanostores/react";
 import type { Habit } from "@data/habits";
-import { habits } from "@stores/habits";
+import { addHabit, habits } from "@stores/habits";
 
 interface Props {
   habit: Habit;
 }
 
-export default function HabitCard({ habit }: Props) {
-  const { name, icon } = habit;
+function HabitCard({ habit }: Props) {
   const $habits = useStore(habits);
-
-  const handleHabit = (habit: Habit) => {
-    habits.set([...$habits, habit]);
-    console.log($habits);
-  };
 
   return (
     <div
       className="card flex h-24 items-center justify-center bg-base-300 p-4 transition-colors hover:bg-base-200"
-      onClick={() => handleHabit(habit)}
+      onClick={() => addHabit(habit)}
     >
-      <span className="flex basis-full items-center text-lg">{icon}</span>
-      <h2 className="flex basis-full items-start text-center">{name}</h2>
+      <span className="flex basis-full items-center text-lg">
+        {habit?.icon}
+      </span>
+      <h2 className="flex basis-full items-start text-center">{habit?.name}</h2>
     </div>
   );
 }
+export default HabitCard;
--- src/stores/habits.ts
+++ src/stores/habits.ts
@@ -1,4 +1,8 @@
-import { atom } from "nanostores";
+import { atom, map } from "nanostores";
 import type { Habit } from "@data/habits";
 
-export const habits = atom<Habit[]>([]);
+export const habits = map<Record<string, Habit>>({});
+
+export const addHabit = (habit: Habit) => {
+  habits.setKey(crypto.randomUUID(), habit);
+};
