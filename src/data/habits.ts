539394b2f451c8e89477c7fbb51713ed3e4d95c5
Cake <cakemancerr@gmail.com>
2023-01-08 22:55:10 +0100

    chore: add id to habits

--- src/data/habits.ts
+++ src/data/habits.ts
@@ -1,13 +1,16 @@
+import crypto from "crypto";
+
 export type Habit = {
-	name: string;
-	icon: string;
-}
+  name: string;
+  icon: string;
+  id: string;
+};
 
 export const habits: Habit[] = [
-  { name: "Meditate", icon: "🙏" },
-  { name: "Exercise", icon: "🏋️‍♂️" },
-  { name: "Read", icon: "📖" },
-  { name: "Write", icon: "📝" },
-  { name: "Drink Water", icon: "💧" },
-  { name: "Sleep Well", icon: "🛌" },
+  { name: "Meditate", icon: "🙏", id: crypto.randomUUID() },
+  { name: "Exercise", icon: "🏋️‍♂️", id: crypto.randomUUID() },
+  { name: "Read", icon: "📖", id: crypto.randomUUID() },
+  { name: "Write", icon: "📝", id: crypto.randomUUID() },
+  { name: "Drink Water", icon: "💧", id: crypto.randomUUID() },
+  { name: "Sleep Well", icon: "🛌", id: crypto.randomUUID() },
 ];
