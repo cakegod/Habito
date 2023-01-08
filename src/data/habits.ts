abe4a41cce799c6523b086e27e4d7d973f6cc97b
Cake <cakemancerr@gmail.com>
2023-01-08 20:31:25 +0100

    feat: add basic habits data

---
+++ src/data/habits.ts
@@ -0,0 +1,8 @@
+export const habits = [
+  { name: "Meditation", icon: "🙏" },
+  { name: "Exercise", icon: "🏋️‍♂️" },
+  { name: "Reading", icon: "📖" },
+  { name: "Journaling", icon: "📝" },
+  { name: "Drinking Water", icon: "💧" },
+  { name: "Getting Enough Sleep", icon: "🛌" },
+];
