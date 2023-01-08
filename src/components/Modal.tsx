150b43b0790760703e8236da9b3dddbc1e4f22f8
Cake <cakemancerr@gmail.com>
2023-01-09 00:13:12 +0100

    feat: more modal content

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -9,20 +9,42 @@ function Modal({ habit }: Props) {
     <>
       <input type="checkbox" id={habit.name} className="modal-toggle" />
       <div className="modal">
-        <div className="modal-box">
+        <div className="modal-box flex max-w-md flex-col gap-2">
+          <label
+            htmlFor={habit.name}
+            className="btn-sm btn-circle btn absolute right-2 top-2 border-none bg-transparent text-lg"
+          >
+            ✕
+          </label>
           <div className="flex gap-2 text-xl">
-            <span>{habit?.icon}</span>
+            <span>{habit.icon}</span>
             <h3 className="font-bold">{habit.name}</h3>
           </div>
-          <p className="py-4">
-            You've been selected for a chance to get one year of subscription to
-            use Wikipedia for free!
-          </p>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">How long?</span>
+            </label>
+            <label className="input-group">
+              <input
+                type="number"
+                required
+                tabIndex={0}
+                min="0"
+                step="5"
+                placeholder="5"
+                className="input-bordered input w-full placeholder:text-base-content/50"
+              />
+              <select className="select bg-base-300 uppercase">
+                <option selected>minutes</option>
+                <option>hours</option>
+              </select>
+            </label>
+          </div>
           <div className="modal-action">
-            <label htmlFor={habit.name} className="btn btn-ghost">
+            <label htmlFor={habit.name} className="btn-ghost btn uppercase">
               Cancel
             </label>
-            <label htmlFor={habit.name} className="btn btn-primary">
+            <label htmlFor={habit.name} className="btn-primary btn uppercase">
               Add habit
             </label>
           </div>
