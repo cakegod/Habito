4e3acaabf91ab7bc045407031da1006bc9e079bd
Cake <cakemancerr@gmail.com>
2023-01-10 18:30:29 +0100

    style: limit width

--- src/components/HabitCard.tsx
+++ src/components/HabitCard.tsx
@@ -8,10 +8,7 @@ interface Props {
 function HabitCard({ habit }: Props) {
   return (
     <>
-      <button
-        className="btn h-24 flex-col "
-        onClick={() => toggleModal(habit)}
-      >
+      <button className="btn h-24 flex-col" onClick={() => toggleModal(habit)}>
         <span className="flex grow items-center text-lg">{habit.icon}</span>
         <h2 className="flex grow items-start text-center">{habit.name}</h2>
       </button>
