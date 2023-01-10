696440f6e6213cbfea2f9cd9cdf9ca25f0099683
Cake <cakemancerr@gmail.com>
2023-01-10 21:32:08 +0100

    feat: add combine data type

--- src/stores/habits.ts
+++ src/stores/habits.ts
@@ -2,19 +2,36 @@ import { atom, map } from "nanostores";
 import type { Habit } from "@data/habits";
 import type { HabitData } from "@components/home/ModalForm";
 
-export const habits = map<Record<string, Habit & HabitData>>({});
+export type HabitStateData = Habit & HabitData;
+export const habits = map<Record<string, HabitStateData>>({});
 
-export const addHabit = (habit: Habit & HabitData) => {
+export const addHabit = (habit: HabitStateData) => {
   habits.setKey(habit.id, habit);
 
   console.log(habits.get());
 };
 
 // Bad temporary fix :/
-export const currentHabit = atom<Habit>({} as Habit);
+export const currentHabit = atom<HabitStateData>({
+  name: "",
+  icon: "",
+  id: "",
+  forms: [],
+  time: {
+    value: 0,
+    type: "minutes",
+  },
+  frequency: {
+    value: 0,
+  },
+  liquid: {
+    value: 0,
+    type: "ml",
+  },
+});
 
 export const isModalOpen = atom(false);
-export const toggleModal = (habit?: Habit) => {
+export const toggleModal = (habit?: HabitStateData) => {
   isModalOpen.set(!isModalOpen.get());
   if (habit) {
     currentHabit.set(habit);
