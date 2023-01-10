8822a7e7c484ae1339b38cc80287b2d2537adcf4
Cake <cakemancerr@gmail.com>
2023-01-11 00:42:11 +0100

    feat: add code habit

--- src/data/habits.ts
+++ src/data/habits.ts
@@ -6,12 +6,13 @@ export type Habit = {
 };
 
 export const habitsData: Habit[] = [
-  { name: "Meditate", icon: "🙏", id: "1", forms: ["time", "frequency"] },
+  { name: "Meditate", icon: "🧘‍♀️", id: "1", forms: ["time", "frequency"] },
   { name: "Exercise", icon: "🏋️‍♂️", id: "2", forms: ["time", "frequency"] },
   { name: "Read", icon: "📖", id: "3", forms: ["time", "frequency"] },
   { name: "Write", icon: "📝", id: "4", forms: ["time", "frequency"] },
   { name: "Drink Water", icon: "💧", id: "5", forms: ["liquid"] },
   { name: "Sleep Well", icon: "🛌", id: "6", forms: ["time", "frequency"] },
+  { name: "Code", icon: "👨‍💻", id: "7", forms: ["time", "frequency"] },
 ];
 
 type Forms = "time" | "frequency" | "liquid";
