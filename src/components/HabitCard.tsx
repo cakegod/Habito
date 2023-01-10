07ff825554827c91f4b812e44d3bd71ab6531cf3
Cake <cakemancerr@gmail.com>
2023-01-10 18:25:03 +0100

    style: habit card refactor

--- src/components/HabitCard.tsx
+++ src/components/HabitCard.tsx
@@ -1,6 +1,5 @@
 import type { Habit } from "@data/habits";
 import { toggleModal } from "@stores/habits";
-import Modal from "./Modal";
 
 interface Props {
   habit: Habit;
@@ -10,15 +9,11 @@ function HabitCard({ habit }: Props) {
   return (
     <>
       <button
-        className="card flex h-24 items-center justify-center bg-base-300 p-4 transition-colors hover:bg-base-200"
+        className="btn h-24 flex-col "
         onClick={() => toggleModal(habit)}
       >
-        <span className="flex basis-full items-center text-lg">
-          {habit.icon}
-        </span>
-        <h2 className="flex basis-full items-start text-center">
-          {habit.name}
-        </h2>
+        <span className="flex grow items-center text-lg">{habit.icon}</span>
+        <h2 className="flex grow items-start text-center">{habit.name}</h2>
       </button>
     </>
   );
