758db90a1a4c4fe2e2e85814f1a8f2f668ca14bc
Cake <cakemancerr@gmail.com>
2023-01-10 23:43:31 +0100

    style: add modal transition

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,27 +2,38 @@ import CloseButton from "@components/home/CloseButton";
 import ModalForm from "@components/home/ModalForm";
 import { useStore } from "@nanostores/react";
 import { currentHabit, isModalOpen } from "@stores/habits";
-import { Dialog } from "@headlessui/react";
+import { Dialog, Transition } from "@headlessui/react";
+import { Fragment } from "react";
 
 function Modal() {
   const $isModalOpen = useStore(isModalOpen);
   const $currentHabit = useStore(currentHabit);
 
   return (
-    <Dialog
-      className="modal modal-open"
-      open={$isModalOpen}
-      onClose={() => isModalOpen.set(false)}
+    <Transition
+      show={$isModalOpen}
+			enter="transition-opacity duration-300"
+			enterFrom="opacity-0"
+			enterTo="opacity-100"
+			leave="transition-opacity duration-300"
+			leaveFrom="opacity-100"
+			leaveTo="opacity-0"
+      as={Fragment}
     >
-      <Dialog.Panel className="modal-box flex max-w-md flex-col gap-2">
-        <CloseButton />
-        <Dialog.Title className="flex items-center text-xl font-bold">
-          <span className="pr-2 text-2xl">{$currentHabit.icon}</span>
-          {$currentHabit.name}
-        </Dialog.Title>
-        <ModalForm habit={$currentHabit} />
-      </Dialog.Panel>
-    </Dialog>
+      <Dialog
+        className="modal modal-open"
+        onClose={() => isModalOpen.set(false)}
+      >
+        <Dialog.Panel className="modal-box flex max-w-md flex-col gap-2">
+          <CloseButton />
+          <Dialog.Title className="flex items-center text-xl font-bold">
+            <span className="pr-2 text-2xl">{$currentHabit.icon}</span>
+            {$currentHabit.name}
+          </Dialog.Title>
+          <ModalForm habit={$currentHabit} />
+        </Dialog.Panel>
+      </Dialog>
+    </Transition>
   );
 }
 
