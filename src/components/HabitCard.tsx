48a9eeb6301209872b7f869be40085a72a2dc41e
Cake <cakemancerr@gmail.com>
2023-01-11 00:58:27 +0100

    style: habit card normal-case

--- src/components/HabitCard.tsx
+++ src/components/HabitCard.tsx
@@ -8,7 +8,10 @@ interface Props {
 function HabitCard({ habit }: Props) {
   return (
     <>
-      <button className="btn h-24 flex-col" onClick={() => toggleModal(habit)}>
+      <button
+        className="btn h-24 flex-col normal-case"
+        onClick={() => toggleModal(habit)}
+      >
         <span className="flex grow items-center text-lg">{habit.icon}</span>
         <h2 className="flex grow items-start text-center">{habit.name}</h2>
       </button>
