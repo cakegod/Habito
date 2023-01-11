542e00f3e1ee07a3f0f7ec805297595777798dc5
Cake <cakemancerr@gmail.com>
2023-01-11 18:31:01 +0100

    style: adjust grid

--- src/components/AddedHabits.tsx
+++ src/components/AddedHabits.tsx
@@ -45,10 +45,10 @@ function calculateLiquid(
 function AddedHabits() {
   const $habits = useStore(habits);
   return $habits.length > 0 ? (
-    <div className="absolute bottom-0 left-0 z-10 grid max-h-[15%] w-full grid-cols-2 gap-2 self-start overflow-auto bg-base-300 p-2 md:grid-cols-3 lg:grid-cols-6">
+    <div className="absolute bottom-0 left-0 z-10 grid max-h-[15%] w-full grid-cols-[repeat(auto-fill,_minmax(200px,_1fr))] gap-2 self-start overflow-auto bg-base-300 p-2">
       {$habits.map((habit) => (
         <button
-          className="btn-outline btn  flex h-24 max-h-full grow flex-col gap-2 p-2 normal-case"
+          className="btn flex h-24 max-h-full grow flex-col gap-1 p-2 normal-case"
           onClick={() => toggleModal(habit)}
           key={habit.id}
         >
@@ -58,17 +58,17 @@ function AddedHabits() {
           </div>
           {habit.forms.includes("time") ? (
             <>
-              <span className="badge badge-sm">
+              <span className="badge-info badge-outline badge badge-sm ">
                 {calculateTime(
                   habit.frequency.value,
                   Number(habit.time.value),
                   habit.time.type
                 )}
               </span>
-              <span className="badge badge-sm">{`${habit.frequency.value} times / week`}</span>
+              <span className="badge-info badge-outline badge badge-sm ">{`${habit.frequency.value} times / week`}</span>
             </>
           ) : (
-            <span className="badge badge-sm">
+            <span className="badge-info badge-outline badge badge-sm ">
               {calculateLiquid(Number(habit.liquid.value), habit.liquid.type)}
             </span>
           )}
