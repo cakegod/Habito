1d0e74534b602679a9478ae4770ceacd63c0b727
Cake <cakemancerr@gmail.com>
2023-01-11 17:35:42 +0100

    feat: conditional rendering the main section

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import AddedHabits from "./AddedHabits";
 import HabitsList from "./home/HabitsList";
-import Button from '@components/Button'
 
 function App() {
   const [index, setIndex] = useState<0 | 1>(0);
@@ -16,26 +15,38 @@ function App() {
 
   return (
     <>
-      <div className="w-full max-w-xl grow">
-        <main className="flex w-full max-w-3xl flex-col items-center">
-          <section className="grid w-full grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
-            <HabitsList />
-          </section>
-          <div className="mt-10 flex w-full justify-center">
-            {index === 0 ? (
-              <Button intent="success" size="large">
-								Calculate my habits!
-							</Button>
-            ) : (
-              <button className="btn btn-info btn-lg" onClick={handleBack}>
-                <span className="m-1 rotate-180">➤</span>
-                Back to habit selection
-              </button>
-            )}
+      {index === 0 ? (
+        <>
+          <div className="w-full max-w-xl grow">
+            <main className="flex w-full max-w-3xl flex-col items-center">
+              <section className="grid w-full grid-cols-2 gap-2 md:grid-cols-3 lg:grid-cols-4">
+                <HabitsList />
+              </section>
+              <div className="mt-10 flex w-full justify-center">
+                <button className="btn btn-success btn-lg" onClick={handleNext}>
+                  Calculate my habits!
+                  <span className="m-1 text-xl">🤯</span>
+                </button>
+              </div>
+            </main>
           </div>
-        </main>
-      </div>
-      <AddedHabits />
+          <AddedHabits />
+        </>
+      ) : (
+        <>
+          <div className="w-full max-w-xl grow">
+            <main className="flex w-full max-w-3xl flex-col items-center">
+              // TODO: Habit grid here
+              <div className="mt-10 flex w-full justify-center">
+                <button className="btn btn-info btn-lg" onClick={handleBack}>
+                  <span className="m-1 rotate-180">➤</span>
+                  Back to habit selection
+                </button>
+              </div>
+            </main>
+          </div>
+        </>
+      )}
     </>
   );
 }
