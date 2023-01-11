0866108b81159dc37d628760ad10317270b12345
Cake <cakemancerr@gmail.com>
2023-01-11 20:54:32 +0100

    refactor: util funcs

--- src/util/calculate.ts
+++ src/util/calculate.ts
@@ -1,41 +1,37 @@
 import type { HabitData } from "@components/home/ModalForm";
 
-export function calculateTime(
+const WEEK = 7;
+
+export function calculateMinutesPerDay(
   frequency: number,
   time: number,
   timeType: HabitData["time"]["type"]
 ) {
-  const WEEK = 7;
-  const durationPerDay = Math.ceil((frequency * time) / WEEK);
+  const durationPerDay = (frequency * time) / WEEK;
 
   switch (timeType) {
     case "minutes": {
-      return durationPerDay < 60
-        ? `${durationPerDay} minutes / day`
-        : `${((durationPerDay / 60) * 10) / 10} hours / day`;
+      return durationPerDay;
     }
     case "hours": {
-      return `${(durationPerDay * 10) / 10} hours / day`;
+      return durationPerDay * 60;
     }
   }
 }
 
-export function calculateLiquid(
+export function calculateLiquidPerDay(
   value: number,
   liquidType: HabitData["liquid"]["type"]
 ) {
   const LITER = 1000;
-  const WEEK = 7;
   const VOLUME = value * WEEK;
 
   switch (liquidType) {
     case "ml": {
-      return VOLUME < 1000
-        ? `${VOLUME}ml / week`
-        : `${(VOLUME * WEEK) / LITER}L / week`;
+      return VOLUME / LITER;
     }
     case "l": {
-      return `${VOLUME}L / week`;
+      return VOLUME;
     }
   }
 }
