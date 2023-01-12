0abbe78ce3c700810a412bfdcb11fc6538883c6a
Cake <cakemancerr@gmail.com>
2023-01-12 14:04:01 +0100

    refactor: split HabitsGridCards

--- src/components/app/HabitsGridCards.tsx
+++ src/components/app/HabitsGridCards.tsx
@@ -1,10 +1,6 @@
 import { useStore } from "@nanostores/react";
-import { habits } from "@stores/habits";
+import { habits, HabitStateData } from "@stores/habits";
 import { calculateHoursPerYear } from "@util/calculate";
-
-interface Props {
-  year: number;
-}
 
 const gradients = [
   "bg-gradient-to-r from-sky-400 to-blue-500",
@@ -21,34 +17,58 @@ function pickGradient(index: number) {
 }
 
 // TODO add logic for liquid and custom calculation (like drops of rain)
-export function HabitsGridCards({ year }: Props) {
+export default function HabitsGridCards({ year }: { year: number }) {
   const $habits = useStore(habits);
   return (
     <section className="grid w-full grid-cols-2 gap-2 ">
-      {$habits.map((habit, i) => {
-        const { frequency, time, icon, name } = habit;
-        return (
-          <div
-            className={`card cursor-pointer gap-1 p-4 ${pickGradient(i + 1)}`}
-          >
-            <div className="flex items-center gap-2 font-bold uppercase">
-              <span>{icon}</span>
-              <p className="text-sm text-primary-content">{name}</p>
-            </div>
-            <p className="text-2xl font-bold text-primary-content">
-              {`${
-                Math.round(
-                  calculateHoursPerYear(
-                    frequency.value,
-                    Number(time.value),
-                    time.type
-                  )
-                ) * year
-              } hours`}
-            </p>
-          </div>
-        );
-      })}
+      {$habits.map((habit, index) => (
+        <HabitCard habit={habit} year={year} index={index} />
+      ))}
     </section>
+  );
+}
+
+function HabitCard({
+  habit,
+  year,
+  index,
+}: {
+  habit: HabitStateData;
+  year: number;
+  index: number;
+}) {
+  return (
+    <div className={`card cursor-pointer gap-1 p-4 ${pickGradient(index + 1)}`}>
+      <Title name={habit.name} icon={habit.icon} />
+      <HoursPerYear habit={habit} year={year} />
+    </div>
+  );
+}
+
+function Title({ icon, name }: { icon: string; name: string }) {
+  return (
+    <div className="flex items-center gap-2 font-bold uppercase">
+      <span>{icon}</span>
+      <p className="text-sm text-primary-content">{name}</p>
+    </div>
+  );
+}
+
+function HoursPerYear({
+  habit,
+  year,
+}: {
+  habit: HabitStateData;
+  year: number;
+}) {
+  const { frequency, time } = habit;
+  return (
+    <p className="text-2xl font-bold text-primary-content">
+      {`${
+        Math.round(
+          calculateHoursPerYear(frequency.value, Number(time.value), time.type)
+        ) * year
+      } hours`}
+    </p>
   );
 }
