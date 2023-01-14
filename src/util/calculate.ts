6d8e07e8a14465ee6443f176558c31e4ad35a45a
Cake <cakemancerr@gmail.com>
2023-01-14 22:57:27 +0100

    feat: add generic calculate

--- src/util/calculate.ts
+++ src/util/calculate.ts
@@ -9,6 +9,7 @@ const UNITS = {
   hours: 60,
   ml: 1,
   l: 1000,
+  generic: 1,
 };
 
 export function calculateDaily(
@@ -50,7 +51,7 @@ export function formatLiquidPerWeek(
   dailyValue: number,
   unit: keyof typeof UNITS
 ) {
-  const result = calculateDaily(7, dailyValue, unit);
+  const result = calculateDaily(7, dailyValue, unit) * DAYS_PER_WEEK;
   return result >= 1000
     ? `${result / ML_PER_LITER}L / week`
     : `${result}mL / week`;
@@ -62,7 +63,10 @@ export function formatLiquidPerYear(
   year: number
 ) {
   const result = calculateDaily(7, dailyValue, unit) * DAYS_PER_YEAR * year;
-  return result >= 1000
-    ? `${result / ML_PER_LITER}L`
-    : `${result}mL`;
+  return result >= 1000 ? `${result / ML_PER_LITER}L` : `${result}mL`;
+}
+
+export function formatGenericPerYear(dailyValue: number, year: number) {
+  const result =
+    calculateDaily(7, dailyValue, "generic") * DAYS_PER_WEEK * year;
 }
