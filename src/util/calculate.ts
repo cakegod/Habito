e260828ca55cf84957cd35f5ce3b0bf83251b785
Cake <cakemancerr@gmail.com>
2023-01-14 22:21:59 +0100

    refactor: calculate

--- src/util/calculate.ts
+++ src/util/calculate.ts
@@ -1,71 +1,68 @@
-import type { CombinedHabitState } from "@components/modal/ModalForm";
-
 const MINS_PER_HOUR = 60;
 const HOURS_PER_DAY = 24;
 const DAYS_PER_WEEK = 7;
 const DAYS_PER_YEAR = 365;
+const ML_PER_LITER = 1000;
 
-type TimeUnit = "minutes" | "hours";
-type LiquidUnit = "ml" | "l";
+const UNITS = {
+  minutes: 1,
+  hours: 60,
+  ml: 1,
+  l: 1000,
+};
 
-// FIXME: infer the options
-
-export function calculateMinutesPerDay(
+export function calculateDaily(
   frequency: number,
-  time: number,
-  timeType: TimeUnit
+  dailyValue: number,
+  unit: keyof typeof UNITS
 ) {
-  const durationPerDay = (frequency * time) / DAYS_PER_WEEK;
-  console.log(frequency, time, timeType);
-
-  switch (timeType) {
-    case "minutes": {
-      return durationPerDay;
-    }
-    case "hours": {
-      return durationPerDay * 60;
-    }
-  }
+  const result = ((frequency * dailyValue) / DAYS_PER_WEEK) * UNITS[unit];
+  return result;
 }
 
-export function composeTimePerYear(
+export function formatTimePerWeek(
   frequency: number,
-  time: number,
-  timeType: TimeUnit,
+  dailyValue: number,
+  unit: keyof typeof UNITS
+) {
+  const result =
+    Math.round(
+      calculateDaily(frequency, dailyValue, unit) * DAYS_PER_WEEK * 10
+    ) / 10;
+  return result >= 60
+    ? `${result / MINS_PER_HOUR} hours / week`
+    : `${result} min / week`;
+}
+
+export function formatTimePerYear(
+  frequency: number,
+  dailyValue: number,
+  unit: keyof typeof UNITS,
   year: number
 ) {
-  const hoursPerYear =
-    ((calculateMinutesPerDay(frequency, time, timeType) * DAYS_PER_YEAR) /
-      MINS_PER_HOUR) *
-    year;
-  if (hoursPerYear >= HOURS_PER_DAY) {
-    return `${Math.round(hoursPerYear / HOURS_PER_DAY)} day${
-      hoursPerYear > 1 ? "s" : ""
-    }`;
-  }
-  return `${Math.round(hoursPerYear * 10) / 10} hours`;
+  const result =
+    (calculateDaily(frequency, dailyValue, unit) * DAYS_PER_YEAR * year) /
+    MINS_PER_HOUR;
+  return `${result} hours`;
 }
 
-export function calculateLiquidPerDay(value: number, liquidType: LiquidUnit) {
-  const LITER = 1000;
-  const VOLUME = value * DAYS_PER_WEEK;
-
-  switch (liquidType) {
-    case "ml": {
-      return VOLUME / LITER;
-    }
-    case "l": {
-      return VOLUME;
-    }
-  }
-}
-
-export function composeHoursPerWeek(
-  frequency: number,
-  time: number,
-  timeType: TimeUnit
+export function formatLiquidPerWeek(
+  dailyValue: number,
+  unit: keyof typeof UNITS
 ) {
-  const minPerDay = calculateMinutesPerDay(frequency, time, timeType);
-  const hoursPerWeek = Math.round(((minPerDay * 7) / 60) * 10) / 10;
-  return `${hoursPerWeek} hours per week`;
+  const result = calculateDaily(7, dailyValue, unit);
+  return result >= 1000
+    ? `${result / ML_PER_LITER}L / week`
+    : `${result}mL / week`;
+}
+
+export function formatLiquidPerYear(
+  dailyValue: number,
+  unit: keyof typeof UNITS,
+  year: number
+) {
+  const result = calculateDaily(7, dailyValue, unit) * DAYS_PER_YEAR * year;
+  return result >= 1000
+    ? `${result / ML_PER_LITER}L`
+    : `${result}mL`;
 }
