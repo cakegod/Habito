9d0f5dbf5a309dcc3660406b1695b2fa1ab38ae0
Cake <cakemancerr@gmail.com>
2023-01-15 23:59:23 +0100

    refactor: simply inputs data

--- src/data/inputs.ts
+++ src/data/inputs.ts
@@ -1,103 +1,72 @@
-type InputBase = {
+type GetPropValues<T, B extends keyof T[keyof T]> = T[keyof T][B];
+
+export type Base = Readonly<{
   name: string;
   label: string;
-};
+}>;
 
-type SimpleInputArg = {
-  placeholder: string;
+export type InputGroup = {
+  inputCategory: "inputGroup";
   value: string;
   type: "number" | "text";
-};
-
-type SelectDropdownArg = {
-  options: [number, string][];
-  selectedOption: number;
-};
-
-type SelectGroupArg = {
-  options: string[];
+  placeholder: string;
+  options: readonly string[];
   selectedOption: string;
 };
 
-type Selects = SelectDropdownArg | SelectGroupArg;
+export type InputSelect = {
+  inputCategory: "inputSelect";
+  value: string;
+  type: "number" | "text";
+  placeholder: string;
+  options: readonly string[];
+  selectedOption: string;
+};
 
-function inputBase<T>(
-  base: InputBase,
-  ...inputs: (Selects | SimpleInputArg)[]
-): InputBase & T {
-  return Object.assign(base, ...inputs);
-}
+export type InputCategory = GetPropValues<typeof inputs, "inputCategory">;
 
-function inputGroup(input: SimpleInputArg, select: SelectGroupArg) {
-  return {
-    inputCategory: "inputGroup" as const,
-    ...input,
-    ...select,
-  };
-}
+export type InputType = InputGroup | InputSelect;
+export type Input = Base & InputType;
 
-function inputDropdown(select: SelectDropdownArg) {
-  return {
-    inputCategory: "inputDropdown" as const,
-    ...select,
-  };
-}
-
-export type InputGroup = ReturnType<typeof inputGroup>;
-export type InputDropdown = ReturnType<typeof inputDropdown>;
-export type Input = ReturnType<typeof inputBase<InputGroup | InputDropdown>>;
-
-const inputs = {
-  liquid: inputBase<InputGroup>(
-    { name: "liquid", label: "Liquid drank per day" },
-    inputGroup(
-      {
-        value: "",
-        type: "number",
-        placeholder: "50",
-      },
-      { options: ["ml", "l"], selectedOption: "ml" }
-    )
-  ),
-
-  time: inputBase<InputGroup>(
-    { name: "time", label: "Time spent" },
-    inputGroup(
-      {
-        value: "",
-        type: "number",
-        placeholder: "5",
-      },
-      { options: ["minutes", "hours"], selectedOption: "minutes" }
-    )
-  ),
-
-  frequency: inputBase<InputDropdown>(
-    { name: "frequency", label: "Frequency" },
-    inputDropdown({
-      options: [
-        [1, "1 time per week"],
-        [2, "2 times per week"],
-        [3, "3 times per week"],
-        [4, "4 times per week"],
-        [5, "5 times per week"],
-        [6, "6 times per week"],
-        [7, "Every day 🚀"],
-      ],
-      selectedOption: 3,
-    })
-  ),
-  cigarettes: inputBase<InputGroup>(
-    { name: "generic", label: "cigarettes per day" },
-    inputGroup(
-      {
-        value: "",
-        type: "number",
-        placeholder: "1",
-      },
-      { options: ["cigarettes"], selectedOption: "cigarettes" }
-    )
-  ),
-} satisfies Record<string, Input>;
-
-export default inputs;
+export const inputs = {
+  liquid: {
+    inputCategory: "inputGroup",
+    name: "liquid",
+    label: "Liquid drank per day",
+    value: "",
+    placeholder: "50",
+    type: "number",
+    options: ["ml", "l"],
+    selectedOption: "ml",
+  },
+  time: {
+    inputCategory: "inputGroup",
+    name: "time",
+    label: "Time spent",
+    value: "",
+    placeholder: "5",
+    type: "number",
+    options: ["minutes", "hours"],
+    selectedOption: "minutes",
+  },
+  frequency: {
+    inputCategory: "inputSelect",
+    name: "frequency",
+    label: "Time spent",
+    value: "",
+    placeholder: "5",
+    type: "number",
+    options: ["minutes", "hours"],
+    selectedOption: "minutes",
+  },
+  cigarettes: {
+    inputCategory: "inputGroup",
+    name: "generic",
+    label: "Cigarettes per day",
+    value: "",
+    placeholder: "1",
+    type: "number",
+    options: ["cigarettes"],
+    selectedOption: "cigarettes",
+  },
+} as const satisfies Record<string, Input>;
