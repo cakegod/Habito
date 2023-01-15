118fd117ca62c095e30d33c0ab9bbf49aa4ad343
Cake <cakemancerr@gmail.com>
2023-01-15 20:29:12 +0100

    chore: update store types

--- src/stores/habits.ts
+++ src/stores/habits.ts
@@ -1,12 +1,11 @@
 import { atom } from "nanostores";
-import type { CombinedHabitState } from "@components/modal/ModalForm";
 import type { HabitData } from "@data/habits";
 
-export const habits = atom<CombinedHabitState[]>([]);
+export const habits = atom<HabitData[]>([]);
 
 // TODO: Refactor into two separated functions
-export const addHabit = (habit: CombinedHabitState) => {
-  const isPresent = !!habits.get().find((h) => h.id === habit.id);
+export const addHabit = (habit: HabitData) => {
+  const isPresent = habits.get().some((h) => h.id === habit.id);
 
   isPresent
     ? // Updating the object with the argument if is present in the array
@@ -16,19 +15,17 @@ export const addHabit = (habit: CombinedHabitState) => {
   console.log(habits.get());
 };
 
-export const deleteHabit = (habit: CombinedHabitState) => {
+export const deleteHabit = (habit: HabitData) => {
   habits.set(habits.get().filter((h) => h.id !== habit.id));
 
   console.log(habits);
 };
 
-export const currentHabit = atom<CombinedHabitState | HabitData>(
-  {} as HabitData
-);
+export const currentHabit = atom<HabitData>({} as HabitData);
 
 export const isModalOpen = atom(false);
 
-export const toggleModal = (habit?: CombinedHabitState | HabitData) => {
+export const toggleModal = (habit?: HabitData) => {
   isModalOpen.set(!isModalOpen.get());
   if (habit) {
     currentHabit.set(habit);
