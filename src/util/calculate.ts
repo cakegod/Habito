90f4c59221bed982b22711b74b760a5e6a14499f
Cake <cakemancerr@gmail.com>
2023-01-16 17:41:21 +0100

    chore: make calculate func take an obj

--- src/util/calculate.ts
+++ src/util/calculate.ts
@@ -12,63 +12,81 @@ const UNITS = {
   generic: 1,
 };
 
-export function calculateDaily(
-  frequency: number,
-  dailyValue: number,
-  unit: keyof typeof UNITS
-) {
-  const result = ((frequency * dailyValue) / DAYS_PER_WEEK) * UNITS[unit];
+interface Props {
+  frequency: number;
+  dailyValue: number;
+  unit: keyof typeof UNITS | number;
+  year: number;
+}
+
+export function calculateDaily({
+  frequency,
+  dailyValue,
+  unit,
+}: Omit<Props, "year">) {
+  const result =
+    ((frequency * dailyValue) / DAYS_PER_WEEK) *
+    (typeof unit === "string" ? UNITS[unit] : 1);
   return result;
 }
 
-export function formatTimePerWeek(
-  frequency: number,
-  dailyValue: number,
-  unit: keyof typeof UNITS
-) {
+export function formatTimePerWeek({
+  frequency,
+  dailyValue,
+  unit,
+}: Omit<Props, "year">) {
+  console.log(frequency, dailyValue, unit);
+
   const result =
     Math.round(
-      calculateDaily(frequency, dailyValue, unit) * DAYS_PER_WEEK * 10
+      calculateDaily({ frequency, dailyValue, unit }) * DAYS_PER_WEEK * 10
     ) / 10;
   return result >= 60
     ? `${result / MINS_PER_HOUR} hours / week`
     : `${result} min / week`;
 }
 
-export function formatTimePerYear(
-  frequency: number,
-  dailyValue: number,
-  unit: keyof typeof UNITS,
-  year: number
-) {
+export function formatTimePerYear({
+  frequency,
+  dailyValue,
+  unit,
+  year,
+}: Props) {
   const result =
-    (calculateDaily(frequency, dailyValue, unit) * DAYS_PER_YEAR * year) /
+    (calculateDaily({ frequency, dailyValue, unit }) * DAYS_PER_YEAR * year) /
     MINS_PER_HOUR;
   return `${Math.round(result)} hours`;
 }
 
-export function formatLiquidPerWeek(
-  dailyValue: number,
-  unit: keyof typeof UNITS
-) {
-  const result = calculateDaily(7, dailyValue, unit) * DAYS_PER_WEEK;
+export function formatLiquidPerWeek({
+  dailyValue,
+  unit,
+}: Pick<Props, "dailyValue" | "unit">) {
+  const result =
+    calculateDaily({ frequency: 7, dailyValue, unit }) * DAYS_PER_WEEK;
   return result >= 1000
     ? `${result / ML_PER_LITER}L / week`
     : `${result}mL / week`;
 }
 
-export function formatLiquidPerYear(
-  dailyValue: number,
-  unit: keyof typeof UNITS,
-  year: number
-) {
-  const result = calculateDaily(7, dailyValue, unit) * DAYS_PER_YEAR * year;
+export function formatLiquidPerYear({
+  dailyValue,
+  unit,
+  year,
+}: Omit<Props, "frequency">) {
+  const result =
+    calculateDaily({ frequency: 7, dailyValue, unit }) * DAYS_PER_YEAR * year;
   return result >= 1000 ? `${result / ML_PER_LITER}L` : `${result}mL`;
 }
 
-export function formatGenericPerYear(dailyValue: number, year: number) {
+export function formatGenericPerYear({
+  dailyValue,
+  year,
+}: Pick<Props, "dailyValue" | "year">) {
   const result =
-    calculateDaily(7, dailyValue, "generic") * DAYS_PER_YEAR * year;
+    calculateDaily({ frequency: 7, dailyValue, unit: "generic" }) *
+    DAYS_PER_YEAR *
+    year;
 
   return result;
 }
