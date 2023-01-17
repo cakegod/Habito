a33c25819b5b66521ce361323c31218bd8759bc9
Cake <cakemancerr@gmail.com>
2023-01-17 19:33:29 +0100

    chore: remove console logs

--- src/stores/habits.ts
+++ src/stores/habits.ts
@@ -7,18 +7,14 @@ export const habits = atom<HabitData[]>([]);
 export const addHabit = (habit: HabitData) => {
   const isPresent = habits.get().some((h) => h.id === habit.id);
 
+  // Updating the object with the argument if is present in the array
   isPresent
-    ? // Updating the object with the argument if is present in the array
-      habits.set(habits.get().map((h) => (h.id === habit.id ? habit : h)))
+    ? habits.set(habits.get().map((h) => (h.id === habit.id ? habit : h)))
     : habits.set([...habits.get(), habit]);
-
-  console.log(habits.get());
 };
 
 export const deleteHabit = (habit: HabitData) => {
   habits.set(habits.get().filter((h) => h.id !== habit.id));
-
-  console.log(habits);
 };
 
 export const currentHabit = atom<HabitData>({} as HabitData);
