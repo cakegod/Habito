ea8d4dfc61e33757b1e40b1560e4cfd0c6f2d769
Cake <cakemancerr@gmail.com>
2023-01-24 23:29:07 +0100

    refactor: improve habits type

--- src/data/habits.ts
+++ src/data/habits.ts
@@ -1,34 +1,28 @@
-import { inputs } from "./inputs";
+import { inputs as input } from "./inputs";
 import { nanoid } from "nanoid";
-
-export type HabitData = Readonly<{
-  name: string;
-  icon: string;
-  id: string;
-  inputs: ReadonlyArray<(typeof inputs)[keyof typeof inputs]>;
-  avoid: boolean;
-}>;
 
 export type HabitsNames = (typeof habitsData)[number]["name"];
 
 function createHabit<Name extends string>(
   name: Name,
-  icon: HabitData["icon"],
-  inputs: HabitData["inputs"],
-  avoid: HabitData["avoid"] = false
-): Omit<HabitData, "name"> & { name: Name } {
+  icon: string,
+  inputs: ReadonlyArray<(typeof input)[keyof typeof input]>,
+  avoid: boolean = false
+) {
   return { name, icon, inputs, avoid, id: nanoid() };
 }
 
+export type HabitData = ReturnType<typeof createHabit>;
+
 // TODO: add more habits
 export const habitsData = [
-  createHabit("Meditate", "🧘‍♀️", [inputs.time, inputs.frequency]),
-  createHabit("Exercise", "🏋️‍♂️", [inputs.time, inputs.frequency]),
-  createHabit("Read", "📖", [inputs.time, inputs.frequency]),
-  createHabit("Write", "📝", [inputs.time, inputs.frequency]),
-  createHabit("Drink Water", "💧", [inputs.liquid]),
-  createHabit("Code", "👨‍💻", [inputs.time, inputs.frequency]),
-  createHabit("Smoke Addiction", "🚬", [inputs.cigarettes], true),
-  createHabit("Smartphone Addiction", "📱", [inputs.smartphone], true),
-  createHabit("Learn Language", "🌎", [inputs.time, inputs.frequency]),
+  createHabit("Meditate", "🧘‍♀️", [input.time, input.frequency]),
+  createHabit("Exercise", "🏋️‍♂️", [input.time, input.frequency]),
+  createHabit("Read", "📖", [input.time, input.frequency]),
+  createHabit("Write", "📝", [input.time, input.frequency]),
+  createHabit("Drink Water", "💧", [input.liquid]),
+  createHabit("Code", "👨‍💻", [input.time, input.frequency]),
+  createHabit("Smoke Addiction", "🚬", [input.cigarettes], true),
+  createHabit("Smartphone Addiction", "📱", [input.smartphone], true),
+  createHabit("Learn Language", "🌎", [input.time, input.frequency]),
 ].sort((a) => (a.avoid ? 1 : -1)) satisfies Readonly<HabitData[]>;
