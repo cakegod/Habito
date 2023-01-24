f6a1d54581e41ae961cdfd0c1f41b522b28ad118
Cake <cakemancerr@gmail.com>
2023-01-24 23:28:58 +0100

    refactor: abstracted inputs and types

--- src/data/inputs.ts
+++ src/data/inputs.ts
@@ -1,64 +1,82 @@
 // HELPER
 type GetPropValues<T, B extends keyof T[keyof T]> = T[keyof T][B];
 
-export type InputNames = "liquid" | "frequency" | "time" | "generic";
-export type InputCategory = GetPropValues<typeof inputs, "inputCategory">;
+function createInputBase<Name extends string>(
+  name: Name,
+  label: string,
+  value: string = "",
+  type: "number" | "text" = "number"
+) {
+  return { name, label, value, type };
+}
+
+function createInputGroup<
+  Name extends string,
+  Option extends string,
+  SelectedOption extends Option
+>(
+  name: Name,
+  label: string,
+  placeholder: string,
+  options: [Option, string][],
+  selectedOption: SelectedOption
+) {
+  return Object.assign({}, createInputBase(name, label), {
+    inputCategory: "inputGroup" as const,
+    placeholder,
+    options,
+    selectedOption,
+  });
+}
+
+function createInputSelect<
+  Name extends string,
+  Option extends number,
+  SelectedOption extends Option
+>(
+  name: Name,
+  label: string,
+  options: [Option, string][],
+  selectedOption: SelectedOption
+) {
+  return Object.assign({}, createInputBase(name, label), {
+    inputCategory: "inputSelect" as const,
+    options,
+    selectedOption,
+  });
+}
+
+export type InputGroup = ReturnType<typeof createInputGroup>;
+export type InputSelect = ReturnType<typeof createInputSelect>;
 export type Input = InputGroup | InputSelect;
-
-export interface Base {
-  name: InputNames;
-  label: string;
-  value: string;
-  type: "number" | "text";
-}
-
-export interface InputGroup extends Base {
-  inputCategory: "inputGroup";
-  placeholder: string;
-  options: ReadonlyArray<readonly [string, string]>;
-  selectedOption: string;
-}
-
-export interface InputSelect extends Base {
-  inputCategory: "inputSelect";
-  options: ReadonlyArray<readonly [number, string]>;
-  selectedOption: string | number;
-}
+export type InputNames = GetPropValues<typeof inputs, "name">;
+export type InputCategory = GetPropValues<typeof inputs, "inputCategory">;
 
 export const inputs = {
-  liquid: {
-    inputCategory: "inputGroup",
-    name: "liquid",
-    label: "Liquid drank per day",
-    value: "",
-    placeholder: "50",
-    type: "number",
-    options: [
+  liquid: createInputGroup(
+    "liquid",
+    "Liquid Drank per day",
+    "50",
+    [
       ["ml", "ml"],
       ["l", "l"],
     ],
-    selectedOption: "ml",
-  },
-  time: {
-    inputCategory: "inputGroup",
-    name: "time",
-    label: "Time spent",
-    value: "",
-    placeholder: "5",
-    type: "number",
-    options: [
+    "ml"
+  ),
+  time: createInputGroup(
+    "time",
+    "Time spent",
+    "5",
+    [
       ["minutes", "minutes"],
       ["hours", "hours"],
     ],
-    selectedOption: "minutes",
-  },
-  frequency: {
-    inputCategory: "inputSelect",
-    name: "frequency",
-    label: "Time spent",
-    value: "",
-    type: "number",
-    options: [
+    "minutes"
+  ),
+  frequency: createInputSelect(
+    "frequency",
+    "Time spent",
+    [
       [1, "1 time per week"],
       [2, "2 times per week"],
       [3, "3 times per week"],
@@ -67,26 +85,20 @@ export const inputs = {
       [6, "6 times per week"],
       [7, "Every day 🚀"],
     ],
-    selectedOption: 3,
-  },
-  cigarettes: {
-    inputCategory: "inputGroup",
-    name: "generic",
-    label: "Cigarettes you want to avoid per day",
-    value: "",
-    placeholder: "1",
-    type: "number",
-    options: [["generic", "cigarettes"]],
-    selectedOption: "generic",
-  },
-  smartphone: {
-    inputCategory: "inputGroup",
-    name: "generic",
-    label: "Hours to avoid per day",
-    value: "",
-    placeholder: "1",
-    type: "number",
-    options: [["generic", "hours"]],
-    selectedOption: "generic",
-  },
-} satisfies Record<string, Input>;
+    3
+  ),
+  cigarettes: createInputGroup(
+    "generic",
+    "Cigarettes you want to avoid per day",
+    "1",
+    [["generic", "cigarettes"]],
+    "generic"
+  ),
+  smartphone: createInputGroup(
+    "generic",
+    "Hours to avoid per day",
+    "1",
+    [["generic", "hours"]],
+    "generic"
+  ),
+};
