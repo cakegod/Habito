e0975fabcb2ef5edad1d0ee2a3b16c5432baf3c9
Cake <cakemancerr@gmail.com>
2023-02-11 22:32:08 +0100

    refactor: extract inputs

--- src/components/modal/ModalForm.svelte
+++ src/components/modal/ModalForm.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import ModalInputs from "./ModalInputs.svelte";
+
   import type { Habit } from "@data/habits";
   import { toggleModal, addHabit, deleteHabit, habits } from "@stores/habits";
 
@@ -15,54 +17,7 @@
 </script>
 
 <form on:submit|preventDefault={() => handleSubmit(habit)}>
-  {#each state.inputs as input}
-    <div class="form-control">
-      <div class="label">
-        <span class="label-text">{input.label}</span>
-      </div>
-      <label class="input-group">
-        {#if input.inputCategory === "inputSelect"}
-          <select
-            class="select w-full bg-base-200"
-            bind:value={input.selectedOption}
-            name="selectedOption"
-            data-cy="input-dropdown"
-          >
-            {#each input.options as option (option[1])}
-              <option value={option[0]}>
-                {option[1]}
-              </option>
-            {/each}
-          </select>
-        {:else if input.inputCategory === "inputGroup"}
-          <input
-            type="number"
-            required
-            tabIndex={0}
-            min="0"
-            step="1"
-            placeholder={input.placeholder}
-            bind:value={input.value}
-            class="input w-full bg-base-200 placeholder:text-base-content/50"
-            name="value"
-            data-cy="input"
-          />
-          <select
-            class="select bg-base-300 uppercase"
-            bind:value={input.selectedOption}
-            name="selectedOption"
-            data-cy="input-select"
-          >
-            {#each input.options as option (option[0])}
-              <option value={option[0]}>
-                {option[1]}
-              </option>
-            {/each}
-          </select>
-        {/if}
-      </label>
-    </div>
-  {/each}
+  <ModalInputs {habit} />
   <div class="modal-action">
     {#if isPresent}
       <button
---
+++ src/components/modal/ModalInputs.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import type { Habit } from "@data/habits";
+
+  // Check if the current item exists in the added habits
+  export let habit: Habit;
+  let state = habit;
+</script>
+
+{#each state.inputs as input}
+  <div class="form-control">
+    <div class="label">
+      <span class="label-text">{input.label}</span>
+    </div>
+    <label class="input-group">
+      {#if input.inputCategory === "inputSelect"}
+        <select
+          class="select w-full bg-base-200"
+          bind:value={input.selectedOption}
+          name="selectedOption"
+          data-cy="input-dropdown"
+        >
+          {#each input.options as option (option[1])}
+            <option value={option[0]}>
+              {option[1]}
+            </option>
+          {/each}
+        </select>
+      {:else if input.inputCategory === "inputGroup"}
+        <input
+          type="number"
+          required
+          tabIndex={0}
+          min="0"
+          step="1"
+          placeholder={input.placeholder}
+          bind:value={input.value}
+          class="input w-full bg-base-200 placeholder:text-base-content/50"
+          name="value"
+          data-cy="input"
+        />
+        <select
+          class="select bg-base-300 uppercase"
+          bind:value={input.selectedOption}
+          name="selectedOption"
+          data-cy="input-select"
+        >
+          {#each input.options as option (option[0])}
+            <option value={option[0]}>
+              {option[1]}
+            </option>
+          {/each}
+        </select>
+      {/if}
+    </label>
+  </div>
+{/each}
