3fd18cb6a21bb8017fa3b405bcdace43c6fd1832
Cake <cakemancerr@gmail.com>
2023-05-28 19:53:09 +0200

    chore: add eslint for svelte

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,20 +2,21 @@ module.exports = {
   // ...
   extends: [
     // ...
-    "plugin:astro/jsx-a11y-strict",
+    "plugin:svelte/recommended",
+    "plugin:astro/recommended",
   ],
   // ...
   overrides: [
     {
       // Define the configuration for `.astro` file.
-      files: ["*.astro"],
+      files: ["*.astro", "*.svelte"],
       // Allows Astro components to be parsed.
       parser: "astro-eslint-parser",
       // Parse the script in `.astro` as TypeScript by adding the following configuration.
       // It's the setting you need when using TypeScript.
       parserOptions: {
         parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
+        extraFileExtensions: [".astro", ".svelte"],
       },
       rules: {
         // override/add rules settings here, such as:
